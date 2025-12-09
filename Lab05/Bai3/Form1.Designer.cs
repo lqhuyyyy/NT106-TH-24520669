0fc7214ae0b8950345051a7a2db3932191cdf56e
PoPo-789 <quanghuy789po@gmail.com>
2025-12-09 17:37:52 +0700

    Up

---
+++ Lab05/Bai3/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            label3 = new Label();
+            lblTotal = new Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblRecent = new System.Windows.Forms.Label();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Montserrat", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(35, 35);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Montserrat", 12F);
+            label2.Location = new Point(35, 91);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 31);
+            label2.TabIndex = 1;
+            label2.Text = "Password:";
+            label2.Click += label2_Click;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Font = new Font("Montserrat", 12F);
+            txtEmail.Location = new Point(160, 32);
+            txtEmail.Margin = new Padding(4, 5, 4, 5);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(362, 32);
+            txtEmail.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Microsoft Sans Serif", 12F);
+            txtPassword.Location = new Point(160, 90);
+            txtPassword.Margin = new Padding(4, 5, 4, 5);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(362, 30);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Montserrat SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.Location = new Point(614, 35);
+            btnLogin.Margin = new Padding(4, 5, 4, 5);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(156, 87);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "LOGIN";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Montserrat", 10F);
+            label3.Location = new Point(35, 144);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 27);
+            label3.TabIndex = 5;
+            label3.Text = "Total:";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold);
+            lblTotal.Location = new Point(111, 147);
+            lblTotal.Margin = new Padding(4, 0, 4, 0);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(19, 20);
+            lblTotal.TabIndex = 6;
+            lblTotal.Text = "0";
+            lblTotal.Click += lblTotal_Click;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new Point(40, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Recent:";
+            // 
+            // lblRecent
+            // 
+            this.lblRecent.AutoSize = true;
+            this.lblRecent.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecent.Location = new System.Drawing.Point(235, 100);
+            this.lblRecent.Name = "lblRecent";
+            this.lblRecent.Size = new System.Drawing.Size(14, 13);
+            this.lblRecent.TabIndex = 9;
+            this.lblRecent.Text = "0";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            listView1.Font = new Font("Montserrat", 11F);
+            listView1.Location = new Point(39, 200);
+            listView1.Margin = new Padding(4, 5, 4, 5);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(731, 453);
+            listView1.TabIndex = 7;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Email (Subject)";
+            columnHeader1.Width = 350;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "From";
+            columnHeader2.Width = 230;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Thời gian";
+            columnHeader3.Width = 150;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 692);
+            Controls.Add(listView1);
+            Controls.Add(lblTotal);
+            Controls.Add(label3);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Bài 3: Đọc mail POP3";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4; 
+        private System.Windows.Forms.Label lblRecent;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
---
+++ Lab05/Bai3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using MailKit.Net.Pop3;
+using MimeKit;
+
+namespace Bai3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập Email và Password!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var client = new Pop3Client())
+                {
+                    client.Connect("pop.gmail.com", 995, true);
+                    client.Authenticate("recent:" + email, password);
+                    int messageCount = client.Count;
+                    lblTotal.Text = messageCount.ToString();
+                    lblRecent.Text = messageCount.ToString();
+                    listView1.Items.Clear();
+
+                    // Lấy 15 mail gần nhất
+                    int limit = Math.Min(15, messageCount);
+
+                    for (int i = messageCount - 1; i >= messageCount - limit; i--)
+                    {
+                        var message = client.GetMessage(i);
+
+                        ListViewItem item = new ListViewItem(message.Subject);
+                        item.SubItems.Add(message.From.ToString());
+                        item.SubItems.Add(message.Date.ToString("dd/MM/yyyy HH:mm:ss"));
+
+                        listView1.Items.Add(item);
+                    }
+
+                    client.Disconnect(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sai mật khẩu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
